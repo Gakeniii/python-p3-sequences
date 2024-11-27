f73d52e0a5abe28a87796e4957367a8a8a9bff86
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-11-27 12:48:08 +0300

    python fibonacci seq

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+    else:
+        fibonacci_sequence = [0, 1]
+        for i in range(2, length):
+            next_number = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+            fibonacci_sequence.append(next_number)
+    
+    print(str(fibonacci_sequence))
+
+
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
